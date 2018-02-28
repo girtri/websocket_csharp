fb3ce0f01a61b72690f2a198243a1f423c5f4a8d
gerry <girtri@libero.it>
2018-02-28 12:42:56 +0100

    init project

---
+++ WebSocketSrv/HelloServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketSrv
+{
+    class HelloServer
+    {
+        public void Start()
+        {
+	        List<Fleck.IWebSocketConnection> sockets = new List<Fleck.IWebSocketConnection>();
+	        var server = new Fleck.WebSocketServer("ws://127.0.0.1:8181");
+	
+	        server.Start(socket => 
+	        {
+		        socket.OnOpen = () => 
+		        {
+			        Console.WriteLine("Connection open.");
+			        sockets.Add(socket);
+		        };
+		        socket.OnClose = () =>
+		        {
+			        Console.WriteLine("Connection closed.");
+			        sockets.Remove(socket);
+		        };
+		        socket.OnMessage = message => 
+		        {
+			        Console.WriteLine("Client Says: " + message);
+			        sockets.ToList().ForEach(s => s.Send(" client says: " + message));
+		        };
+	        });
+	
+	        string input = Console.ReadLine();
+	        while(input != "exit")
+	        {
+		        sockets.ToList().ForEach(s => s.Send(input));
+		        input = Console.ReadLine();
+	        }
+        }
+    }
+}
